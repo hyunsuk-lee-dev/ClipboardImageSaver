69bcdc6d1372b6ff0fbdec6a5586a78feeeda8c9
hs.lee <hs.lee@tektonspace.com>
2020-12-22 19:37:34 +0900

    Global Hooking Algorithm Change

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,35 +17,43 @@ using System.ComponentModel;
 using System.Drawing.Imaging;
 using Application = System.Windows.Forms.Application;
 using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows.Forms.VisualStyles;
 
 namespace ClipboardImageSaver
 {
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        readonly string ThemeRegistryKey = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
-        readonly Key firstKey = Key.S;
-        readonly Key secondKey = Key.LeftAlt;
 
-        NotifyIcon notifyIcon;
-        ContextMenuStrip notifyContextMenu;
-        ClipboardHelper clipboardHelper;
-        bool previousFirstKeyToggled = true;
-        bool previousSecondKeyToggled = true;
+        private readonly int hotkeyId = 31197;
+        private readonly uint altModifier = 0x0001;
+        private readonly uint virtualKeyS = 0x53;
+        private readonly string ThemeRegistryKey = @"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
 
-        
+        private NotifyIcon notifyIcon;
+        private ContextMenuStrip notifyContextMenu;
+        private ClipboardHelper clipboardHelper;
+
+        private HwndSource hwndSource;
+        private WindowInteropHelper helper;
+
+        // DLL libraries used to manage hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vlc);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
         public MainWindow()
         {
-            
             InitializeComponent();
 
             clipboardHelper = new ClipboardHelper("Screenshot", @"C:\Users\leehs\Pictures\Screenshots", ImageFormat.Png);
-
-            // Capture Keyobard input for using short-cut key.
-            CompositionTarget.Rendering += OnCompositionTargetRendering;
 
             notifyContextMenu = new ContextMenuStrip();
 
@@ -56,39 +64,66 @@ namespace ClipboardImageSaver
 
             notifyIcon = new NotifyIcon();
 
-            
             // 0 is Dark, 1 is Light.
             int theme = (int)Registry.GetValue(ThemeRegistryKey, "SystemUsesLightTheme", string.Empty);
 
-            notifyIcon.Icon =  theme == 1 ? Properties.Resources.ClipboardBlack : Properties.Resources.ClipboardWhite;
+            notifyIcon.Icon = theme == 1 ? Properties.Resources.ClipboardBlack : Properties.Resources.ClipboardWhite;
             notifyIcon.Visible = true;
             notifyIcon.ContextMenuStrip = notifyContextMenu;
             notifyIcon.Text = "Clipboard Image Saver";
 
             notifyIcon.DoubleClick += OnNotifyIconDoubleClicked;
-
-            Hide();
-
-            previousFirstKeyToggled = Keyboard.IsKeyToggled(firstKey);
-            previousSecondKeyToggled = Keyboard.IsKeyToggled(secondKey);
         }
 
-        private void OnCompositionTargetRendering(object sender, EventArgs e)
+        protected override void OnSourceInitialized(EventArgs e)
         {
-            bool firstKeydown = Keyboard.IsKeyDown(firstKey) && previousFirstKeyToggled != Keyboard.IsKeyToggled(firstKey);
-            bool secondKeydown = Keyboard.IsKeyDown(secondKey) && previousSecondKeyToggled != Keyboard.IsKeyToggled(secondKey);
+            base.OnSourceInitialized(e);
 
-            if(firstKeydown)
-                previousFirstKeyToggled = Keyboard.IsKeyToggled(firstKey);
+            helper = new WindowInteropHelper(this);
+            hwndSource = HwndSource.FromHwnd(helper.Handle);
+            hwndSource.AddHook(new HwndSourceHook(HwndHook));
+            RegisterHotKey();
+        }
 
-            if(secondKeydown)
-                previousSecondKeyToggled = Keyboard.IsKeyToggled(secondKey);
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            Hide();
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            hwndSource.RemoveHook(new HwndSourceHook(HwndHook));
+            hwndSource = null;
+            UnregisterHotKey();
+            base.OnClosed(e);
+        }
 
-            if((firstKeydown && Keyboard.IsKeyDown(secondKey)) || (secondKeydown && Keyboard.IsKeyDown(firstKey)))
+        private void RegisterHotKey()
+        {
+            RegisterHotKey(helper.Handle, hotkeyId, altModifier, virtualKeyS);
+        }
+
+        private void UnregisterHotKey()
+        {
+            UnregisterHotKey(helper.Handle, hotkeyId);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            int hotkeyMessage = 0x0312;
+
+            if(msg == hotkeyMessage && wParam.ToInt32() == hotkeyId)
             {
-                clipboardHelper.SaveClipboardImage();
+                OnHotKeyPressed();
+                handled = true;
             }
 
+            return IntPtr.Zero;
+        }
+
+        private void OnHotKeyPressed()
+        {
+            clipboardHelper.SaveClipboardImage();
         }
 
         private void OnExitItemClicked(object sender, EventArgs e)
